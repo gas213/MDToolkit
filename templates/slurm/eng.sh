cf122433418f4d20f3640fcdd46902d1e82543ac
gas213 <saintgeorgery@gmail.com>
2025-03-05 18:42:04 -0500

    Add SLURM .sh templates

---
+++ templates/slurm/eng.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# REPLACE THE {{FIELDS}}
+# Max run time on eng is 3 days
+
+#SBATCH --partition=eng
+#SBATCH --time=3-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=22
+#SBATCH --job-name {{job_name}}
+#SBATCH --output="job.%j.%N.out"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user={{email}}
+
+# Specify legacy modules (required after Sol upgrade Jan 2025)
+# NOTE: You must run clearLmod command in the terminal before queueing this job
+# NOTE: Infiniband no longer works with legacy modules; can only run on one node
+source /share/Apps/legacy.sh
+
+module purge
+module load intel/2021.3.0
+module load mvapich2/2.3.4
+module load lammps/20210310
+
+cd ${SLURM_SUBMIT_DIR}
+
+mpirun -np $SLURM_NTASKS `which lmp` -in {{in_file}}
+ 
+exit
---
+++ templates/slurm/rapids.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# REPLACE THE {{FIELDS}}
+# Max run time on rapids is 3 days
+
+#SBATCH --partition=rapids
+#SBATCH --time=3-00:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=64
+#SBATCH --job-name {{job_name}}
+#SBATCH --output="job.%j.%N.out"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user={{email}}
+
+module purge
+module load gcc/12.4.0
+module load openmpi/5.0.5
+module load lammps/20240829.1
+
+cd ${SLURM_SUBMIT_DIR}
+
+mpirun -np $SLURM_NTASKS `which lmp` -in {{in_file}}
+ 
+exit
