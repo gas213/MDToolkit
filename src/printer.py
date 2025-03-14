fe15104d8cb9a7ac2171eef4d5c9fb236b232338
gas213 <saintgeorgery@gmail.com>
2025-03-14 14:57:49 -0400

    Switch to f-strings

--- src/printer.py
+++ src/printer.py
@@ -3,51 +3,43 @@ import datetime
 from sanity_checks import atoms_within_box, total_atom_count, density_profile_atom_count
 
 def print_title(data_path):
-    text = "ANALYSIS OF DATA FILE LOCATED AT: " + data_path
-    text += "\nPerformed: " + str(datetime.datetime.now())
-    return text
+    return f"ANALYSIS OF DATA FILE LOCATED AT: {data_path}\nPerformed: {datetime.datetime.now()}"
 
 def print_header(header):
-    text = "\n\nHeader data:"
-    for name, val in header._asdict().items(): text += "\n" + name + ": " + str(val)
+    text = f"\n\nHeader data:"
+    for name, val in header._asdict().items(): text += f"\n{name}: {val}"
     return text
 
 def print_atom_extremes(atom_extremes):
-    text = "\n\nMost extreme atom coordinates:"
-    text += "\n" + str(atom_extremes)
-    return text
+    return f"\n\nMost extreme atom coordinates:\n{atom_extremes}"
 
 def print_sanity_checks(header, atom_extremes, atoms, atom_count_profiles):
-    text = "\n\nSanity checks:"
-    text += "\n" + atoms_within_box(header.box, atom_extremes)
-    text += "\n" + total_atom_count(header, len(atoms))
-    text += "\n" + density_profile_atom_count(header, atom_count_profiles)
+    text = f"\n\nSanity checks:"
+    text += f"\n{atoms_within_box(header.box, atom_extremes)}"
+    text += f"\n{total_atom_count(header, len(atoms))}"
+    text += f"\n{density_profile_atom_count(header, atom_count_profiles)}"
     return text
 
 def print_salt_concentration(salt_concentration):
-    return f"\n\nSalt concentration: {salt_concentration * 100 : 0.3f}%"
+    return f"\n\nSalt concentration:\n{salt_concentration * 100 :0.3f}%"
 
 def print_vapor_count(vapor_count):
-    text = "\n\nVapor count: "
-    text += "\n" + str(vapor_count)
-    return text
+    return f"\n\nVapor count:\n{vapor_count}"
 
 def print_droplet_center(droplet_center):
-    text = "\n\nDroplet center of mass: "
-    text += "\n" + str(droplet_center)
-    return text
+    return f"\n\nDroplet center of mass:\n{droplet_center}"
 
 def print_density_profiles(atom_count_profiles, name):
-    text = "\n\nProfile of " + name + " normalized density vs truncated radius, based on droplet center of mass:"
-    for key, val in atom_count_profiles.r_density_norm.items(): text += "\n" + str(key) + " " + str(val)
-    text += "\n\nProfile of " + name + " density (atoms/angstrom**3) vs truncated radius, based on droplet center of mass:"
-    for key, val in atom_count_profiles.r_density.items(): text += "\n" + str(key) + " " + str(val)
-    text += "\n\nProfile of " + name + " count vs truncated radius, based on droplet center of mass:"
-    for key, val in atom_count_profiles.r_count.items(): text += "\n" + str(key) + " " + str(val)
-    text += "\n\nProfile of " + name + " count vs truncated x coordinate:"
-    for key, val in atom_count_profiles.x.items(): text += "\n" + str(key) + " " + str(val)
-    text += "\n\nProfile of " + name + " count vs truncated y coordinate:"
-    for key, val in atom_count_profiles.y.items(): text += "\n" + str(key) + " " + str(val)
-    text += "\n\nProfile of " + name + " count vs truncated z coordinate:"
-    for key, val in atom_count_profiles.z.items(): text += "\n" + str(key) + " " + str(val)
+    text = f"\n\nProfile of {name} normalized density vs truncated radius, based on droplet center of mass:"
+    for key, val in atom_count_profiles.r_density_norm.items(): text += f"\n{key} {val}"
+    text += f"\n\nProfile of {name} density (atoms/angstrom**3) vs truncated radius, based on droplet center of mass:"
+    for key, val in atom_count_profiles.r_density.items(): text += f"\n{key} {val}"
+    text += f"\n\nProfile of {name} count vs truncated radius, based on droplet center of mass:"
+    for key, val in atom_count_profiles.r_count.items(): text += f"\n{key} {val}"
+    text += f"\n\nProfile of {name} count vs truncated x coordinate:"
+    for key, val in atom_count_profiles.x.items(): text += f"\n{key} {val}"
+    text += f"\n\nProfile of {name} count vs truncated y coordinate:"
+    for key, val in atom_count_profiles.y.items(): text += f"\n{key} {val}"
+    text += f"\n\nProfile of {name} count vs truncated z coordinate:"
+    for key, val in atom_count_profiles.z.items(): text += f"\n{key} {val}"
     return text
--- src/sanity_checks.py
+++ src/sanity_checks.py
@@ -2,14 +2,14 @@ def atoms_within_box(box, atom_extremes):
     result = (box.xlo <= atom_extremes.xlo and box.xhi >= atom_extremes.xhi and
               box.ylo <= atom_extremes.ylo and box.yhi >= atom_extremes.yhi and
               box.zlo <= atom_extremes.zlo and box.zhi >= atom_extremes.zhi)
-    return "All atoms positioned with box: " + str(result)
+    return f"All atoms positioned with box: {result}"
 
 def total_atom_count(header, len_atoms):
     result = (header.atom_count == len_atoms)
-    return "Atom count in header matches number of atom records: " + str(result)
+    return f"Atom count in header matches number of atom records: {result}"
 
 def density_profile_atom_count(header, profiles):
     result = (header.atom_count == sum(profiles.x.values()) and
               header.atom_count == sum(profiles.y.values()) and
               header.atom_count == sum(profiles.z.values()))
-    return "Box-wise density profiles each include every atom: " + str(result)
+    return f"Box-wise density profiles each include every atom: {result}"
