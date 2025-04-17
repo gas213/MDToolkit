330e3c41408d50c6582e23346e1691d6c5b5a603
gas213 <saintgeorgery@gmail.com>
2025-04-17 18:11:43 -0400

    Add print statements to show status

--- src/__main__.py
+++ src/__main__.py
@@ -12,20 +12,30 @@ from md_readers.reader_manager import read_header, read_atoms
 from make_directories import make_directories
 import printer
 
+print("Reading config...")
 config = ConfigReader(read_config_path(sys.argv))
+print("Making directories...")
 dir_write = make_directories(config.data_path)
+print("Reading data header...")
 header = read_header(config.data_path)
+print("Reading atoms...")
 atoms = read_atoms(config.data_path)
+print("Finding atom extremes...")
 atom_extremes = find_atom_extremes(atoms)
+print("Calculating salt concentration...")
 salt_concentration = calc_salt_concentration(config, atoms)
+print("Counting vapor molecules...")
 vapor_count = count_vapor_particles(config, atoms)
+print("Calculating droplet center of mass...")
 droplet_center = calc_droplet_center(config, atoms)
+print("Building density profiles...")
 atom_count_profiles = build_density_profiles(config, header, atoms, droplet_center)
 chlorine_count_profiles = build_density_profiles(config, header, atoms, droplet_center, 4)
 sodium_count_profiles = build_density_profiles(config, header, atoms, droplet_center, 5)
 oxygen_count_profiles = build_density_profiles(config, header, atoms, droplet_center, 6)
 hydrogen_count_profiles = build_density_profiles(config, header, atoms, droplet_center, 7)
 
+print("Writing output file...")
 results = ""
 results += printer.print_title(config.data_path)
 results += printer.print_header(header)
@@ -42,4 +52,4 @@ results += printer.print_density_profiles(hydrogen_count_profiles, "hydrogen")
 
 with open(os.path.join(dir_write, "summary.txt"), "w") as analysis: analysis.write(results)
 
-print("Analysis complete")
+print("ANALYSIS COMPLETE")
