97db850078597b3b5a096c19fb6d54c4dc0c855f
gas213 <saintgeorgery@gmail.com>
2025-05-31 11:34:40 -0400

    Refactor and include core as first bin in spherical profiles

--- src/md_analyses/density_profiles.py
+++ src/md_analyses/density_profiles.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from numpy import ndarray
 
 from constants import four_thirds_pi
 from md_dataclasses.atom import Atom
@@ -8,23 +9,31 @@ from md_dataclasses.header import Header
 from md_dataclasses.vector3d import Vector3D
 from md_readers.config_reader import ConfigReader
 
-def build_histogram(start: float, stop: float, step: float, data: list[float]) -> dict[str, float]:
-    bins = np.arange(start, stop, step)
-    if stop > bins[-1]: bins = np.append(bins, stop) # Append one last bin to cover the remainder (will likely be smaller than the other bins)
+def build_profile(data: list[float], bins: ndarray) -> dict[str, float]:
     hist, _ = np.histogram(data, bins)
     
     labels = [f"[{bins[i]}, {bins[i + 1]})" for i in range(len(bins) - 1)]
     labels[-1] = labels[-1][:-1] + "]"  # The end of the last bin is inclusive
 
-    return {k: v for k, v in zip(labels, hist)}, bins
+    return {k: v for k, v in zip(labels, hist)}
+
+def build_bins_cartesian(start: float, stop: float, step: float) -> ndarray[float]:
+    bins = np.arange(start, stop, step)
+    if stop > bins[-1]: bins = np.append(bins, stop) # Append one last bin to cover the remainder (may be smaller than the other bins)
+    return bins
+
+def build_bins_spherical(start: float, stop: float, step: float) -> ndarray[float]:
+    bins = np.append([0.0], np.arange(start, stop, step)) # First bin is the spherical "core"
+    if stop > bins[-1]: bins = np.append(bins, stop) # Append one last bin to cover the remainder (may be smaller than the other bins)
+    return bins
     
 def build_density_profiles(config: ConfigReader, header: Header, atoms: list[Atom], droplet_com: Vector3D, description: str) -> dict[str, DensityProfile]:
-    x, _ = build_histogram(header.box.lo.x, header.box.hi.x, config.cartesian_profile_step_xyz, [atom.pos.x for atom in atoms])
-    y, _ = build_histogram(header.box.lo.y, header.box.hi.y, config.cartesian_profile_step_xyz, [atom.pos.y for atom in atoms])
-    z, _ = build_histogram(header.box.lo.z, header.box.hi.z, config.cartesian_profile_step_xyz, [atom.pos.z for atom in atoms])
+    x = build_profile([atom.pos.x for atom in atoms], build_bins_cartesian(header.box.lo.x, header.box.hi.x, config.cartesian_profile_step_xyz))
+    y = build_profile([atom.pos.y for atom in atoms], build_bins_cartesian(header.box.lo.y, header.box.hi.y, config.cartesian_profile_step_xyz))
+    z = build_profile([atom.pos.z for atom in atoms], build_bins_cartesian(header.box.lo.z, header.box.hi.z, config.cartesian_profile_step_xyz))
 
-    r_count, r_bins = build_histogram(config.spherical_profile_start_r, config.approx_sphere["R"], config.spherical_profile_step_r,
-                                      [math.sqrt((atom.pos.x - droplet_com.x)**2 + (atom.pos.y - droplet_com.y)**2 + (atom.pos.z - droplet_com.z)**2) for atom in atoms])
+    r_bins = build_bins_spherical(config.spherical_profile_start_r, config.approx_sphere["R"], config.spherical_profile_step_r)
+    r_count = build_profile([math.sqrt((atom.pos.x - droplet_com.x)**2 + (atom.pos.y - droplet_com.y)**2 + (atom.pos.z - droplet_com.z)**2) for atom in atoms], r_bins)
 
     r_bin_volumes = [four_thirds_pi * (r_bins[i + 1]**3 - r_bins[i]**3) for i in range(len(r_bins) - 1)]
     r_density = {k: v for k, v in zip(r_count.keys(), [list(r_count.values())[i] / r_bin_volumes[i] for i in range(len(r_count))])}
