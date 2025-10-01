3b33974c823528fd10c0cf32d2ee717c596c611c
gas213 <saintgeorgery@gmail.com>
2025-10-01 15:26:00 -0400

    Fix printing of test file

--- src/__main__.py
+++ src/__main__.py
@@ -23,6 +23,6 @@ if not os.path.isdir(path_analysis): os.makedirs(path_analysis)
 path_test = os.path.join(path_analysis, "test.txt")
 if os.path.exists(path_test): raise Exception(f"Output file already exists, please rename/move/delete it: {path_test}")
 with open(path_test, "w") as test_file:
-    test_file.writelines([f"{k} {v}" for k, v in state.radial_profile.items()])
+    test_file.writelines([f"{k} {v}\n" for k, v in state.radial_profile.items()])
 
 print("ANALYSIS COMPLETE")
