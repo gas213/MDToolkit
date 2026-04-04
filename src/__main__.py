387cbdd7bbbc98b66cfecfc9285c8ce9b8aa6f8f
gas213 <saintgeorgery@gmail.com>
2026-04-04 18:30:16 -0400

    Remove test code from main

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,3 @@
-import os.path
 import sys
 
 from input_interpreter import InputInterpreter
@@ -13,11 +12,5 @@ with open(input_path, "r") as file:
 state = SessionState()
 interpreter = InputInterpreter(input)
 interpreter.run(state)
-
-# TODO: this is a workaround
-# path_test = os.path.join(path_analysis, "test.txt")
-# if os.path.exists(path_test): raise Exception(f"Output file already exists, please rename/move/delete it: {path_test}")
-# with open(path_test, "w") as test_file:
-#     test_file.writelines([f"{k} {v}\n" for k, v in state.radial_profile.items()])
 
 state.md_logger.log("ANALYSIS COMPLETE")
