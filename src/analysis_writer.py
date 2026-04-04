9cade9a77fa400c413efb293fac4780acf85b2c9
gas213 <saintgeorgery@gmail.com>
2026-04-03 21:55:51 -0400

    Start implementing new analysis file writer

---
+++ src/analysis_writer.py
@@ -0,0 +1,19 @@
+import os.path
+
+from session_state import SessionState
+
+def write_histogram(state: SessionState, write_path_relative: str, append_flag: bool):
+    check_results_path(state)
+    write_path_full: str = os.path.join(str(state.results_path), write_path_relative)
+    write_mode: str = "a" if append_flag else "w"
+    is_appending: bool = append_flag and os.path.isfile(write_path_full)
+
+    with open(write_path_full, write_mode) as file:
+        if is_appending:
+            file.write("\n\n")
+        file.write("Cl neighbors histogram, values only:\n" + " ".join([str(val) for val in state.cl_neighbors_histogram.values()]))
+        file.write("\n\nNa neighbors histogram, values only:\n" + " ".join([str(val) for val in state.na_neighbors_histogram.values()]))
+
+def check_results_path(state: SessionState):
+    if state.results_path is None or not os.path.isdir(state.results_path):
+        raise Exception("Cannot write analysis files before results path has been set.")
--- src/md_commands/salt_histograms_command.py
+++ src/md_commands/salt_histograms_command.py
@@ -1,3 +1,4 @@
+from analysis_writer import write_histogram
 from md_commands.command_interface import Command
 from md_commands.command_validation_helper import CommandValidationHelper
 from md_enums.aggregation_type import AggregationType
@@ -7,9 +8,10 @@ from session_state import SessionState
 class SaltHistogramsCommand(Command):
     def __init__(self, command_name: str, args: list[str]):
         helper = CommandValidationHelper(command_name)
-        helper.check_for_exact_arg_count(args, 2)
+        helper.check_for_exact_arg_count(args, 3)
         self._filter_name = args[0]
         self._aggregation_type = helper.check_categorical_arg(args[1].lower(), AggregationType)
+        self._write_path_relative = args[2]
 
     def execute(self, state: SessionState):
         state.md_logger.log("Building salt histograms...")
@@ -19,14 +21,11 @@ class SaltHistogramsCommand(Command):
         if self._aggregation_type == AggregationType.NONE or state.data_files_index == 0:
             state.cl_neighbors_histogram = cl_neighbors_histogram
             state.na_neighbors_histogram = na_neighbors_histogram
+            write_histogram(state, self._write_path_relative, True)
         elif self._aggregation_type == AggregationType.AVERAGE:
             n_previous = state.data_files_index
             for key in state.cl_neighbors_histogram.keys():
                 state.cl_neighbors_histogram[key] = (n_previous * state.cl_neighbors_histogram[key] + cl_neighbors_histogram[key]) / (n_previous + 1)
             for key in state.na_neighbors_histogram.keys():
                 state.na_neighbors_histogram[key] = (n_previous * state.na_neighbors_histogram[key] + na_neighbors_histogram[key]) / (n_previous + 1)
-
-        state.md_logger.log("Cl neighbors histogram:\n" + str(state.cl_neighbors_histogram)) # TODO: remove
-        state.md_logger.log("Na neighbors histogram:\n" + str(state.na_neighbors_histogram)) # TODO: remove
-        state.md_logger.log("Cl neighbors values only:\n" + " ".join([str(val) for val in state.cl_neighbors_histogram.values()])) # TODO: remove
-        state.md_logger.log("Na neighbors values only:\n" + " ".join([str(val) for val in state.na_neighbors_histogram.values()])) # TODO: remove
+            write_histogram(state, self._write_path_relative, False)
